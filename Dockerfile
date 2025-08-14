3ede65287437f0890c11f74b705a22e0b9e3d1c1
elian-j <eliansteven648@gmail.com>
2025-08-14 15:23:08 -0400

    Práctica Hola Mundo Dockerizada

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+﻿FROM node:20-alpine
+WORKDIR /app
+COPY app.js .
+ENV PORT=3000
+EXPOSE 3000
+CMD ["node","app.js"]
---
+++ app.js
@@ -0,0 +1,9 @@
+﻿const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+  res.end('Hola Mundo DevOps 👋');
+});
+
+server.listen(port, () => console.log(Servidor escuchando en puerto ));
